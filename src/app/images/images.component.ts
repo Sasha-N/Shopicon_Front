d10bb4490e83f8fba9bb359568b3ed99b1483a0b
Таня <t.podkur@g.nsu.ru>
2019-11-09 17:30:19 +0700

    tanya component, service, getImage()

---
+++ src/app/images/images.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-images',
+  templateUrl: './images.component.html',
+  styleUrls: ['./images.component.css']
+})
+export class ImagesComponent implements OnInit {
+  images;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getImages().subscribe((data)=>{
+      this.images = data;
+    });
+  }
+}
